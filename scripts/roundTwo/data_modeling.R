dc0408c57a13445dbf76630cdcd66ef482251754
Landon Lee <landon.lee543@gmail.com>
2023-04-04 18:18:23 -0500

    refactor fitting and remove deprecated functions

--- scripts/roundTwo/data_modeling.R
+++ scripts/roundTwo/data_modeling.R
@@ -3,43 +3,7 @@ library(ggplot2)
 library(purrr)
 library(broom)
 
-
-df_fit <- x030_152 %>%
-    ungroup %>%
-    filter(str_detect(grp, "02 aSf 00 PrLDm")) %>%
-    select(hours, fnorm_value) %>%
-    mutate(hours = as.numeric(hours) / 3600)
-
-df_fit %>%
-    ggplot(aes(x = hours, y = fnorm_value)) +
-        geom_point() +
-        geom_line() +
-        theme(aspect.ratio = 0.4)
-
-fit <- df_fit %>%
-    fit_boltzmann()
-
-df_pred <- tibble(
-    hours = seq(
-        min(df_fit$hours),
-        max(df_fit$hours),
-        length.out = 100
-        )
-    )
-df_pred$fnorm_value <- predict(fit, newdata = df_pred)
-
-ggplot() +
-  geom_point(
-    data = df_fit,
-    aes(x = hours, y = fnorm_value),
-    color = "#FF5733") +
-  geom_line(
-    data = df_pred,
-    aes(x = hours, y = fnorm_value),
-    color = "#1E90FF") +
-  theme(aspect.ratio = 0.4)
-
-############### trying to refactor the above ################
+# Fit the Boltzmann function to each group in the data
 
 fit_boltzmann <- function(df) {
     nls(
@@ -53,49 +17,7 @@ fit_boltzmann <- function(df) {
         )
     )
 }
-print_pred <- function(df, group) {
-    df_fit_ <- df %>%
-        ungroup %>%
-        filter(str_detect(grp, group)) %>%
-        select(hours, fnorm_value) %>%
-        mutate(hours = as.numeric(hours) / 3600)
 
-    fit_ <- df_fit_ %>%
-        fit_boltzmann()
-
-    df_pred_ <- tibble(
-        hours = seq(
-            min(df_fit_$hours),
-            max(df_fit_$hours),
-            length.out = 100
-            )
-        )
-    df_pred_$fnorm_value <- predict(fit_, newdata = df_pred_)
-
-    p <- ggplot() +
-        geom_point(
-            data = df_fit_,
-            aes(x = hours, y = fnorm_value),
-            color = "#FF5733") +
-        geom_line(
-            data = df_pred_,
-            aes(x = hours, y = fnorm_value),
-            color = "#1E90FF") +
-        theme(aspect.ratio = 0.4)
-
-    return(p)
-}
-print_pred(x030_152, "01 aSf 05 PrLDm")
-print_pred(x030_152, "02 aSf 05 PrLDm")
-
-
-
-printplot_temp("01 aSf 00 PrLDm", x030_152)
-
-########################### chat gpt code #########################
-
-
-# Fit the Boltzmann function to each group in the data
 fits <- x030_152 %>%
     ungroup() %>%
     filter(!str_detect(grp, "00 aSf 00 PrLDm|01 aSf 00 PrLDm")) %>%
@@ -105,27 +27,21 @@ fits <- x030_152 %>%
     mutate(fit = map(data, ~ fit_boltzmann(.))) %>%
     mutate(coef = map(fit, tidy)) %>%
     mutate(.fitted = map2(fit, data, ~ augment(.x, newdata = .y)))
+fits
 
-
-
-plot_fits %>% View()
-
-plot_fits %>%
-    ggplot() + # nolint
-    geom_point(aes(x = hours, y = fnorm_value, color = grp)) +
-    geom_line(aes(x = hours, y = .fitted, color = grp)) +
-    coord_cartesian(xlim = c(0, 48)) +
-    theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 0.5)
-
-
-
-plot_boltzmann_fits <- function(filter) {
-    plot_fits_ <- fits %>%
+table_boltzmann_fits <- function(filter) {
+    boltzmann_fits_table_ <- fits %>%
         filter(str_detect(grp, filter)) %>%
         ungroup() %>%
-        unnest(data, .fitted, names_repair = "minimal") %>%
+        unnest(c(data, .fitted), names_repair = "minimal") %>%
         select(hours, grp, fnorm_value, .fitted) %>%
         group_by(grp)
+
+    return(boltzmann_fits_table_)
+}
+
+plot_boltzmann_fits <- function(filter) {
+    plot_fits_ <- table_boltzmann_fits(filter)
 
     p <- plot_fits_ %>%
         ggplot() + # nolint
@@ -142,12 +58,13 @@ plot_boltzmann_fits <- function(filter) {
 
     return(p)
 }
+
 plot_boltzmann_fits("00 aSf") %>% save_plot("00 aSf", "/x030_152/fits/aSf/")
 plot_boltzmann_fits("01 aSf") %>% save_plot("01 aSf", "/x030_152/fits/aSf/")
 plot_boltzmann_fits("02 aSf") %>% save_plot("02 aSf", "/x030_152/fits/aSf/")
 plot_boltzmann_fits("04 aSf") %>% save_plot("04 aSf", "/x030_152/fits/aSf/")
 
-plot_boltzmann_fits("00 aSf") %>% save_plot("05 PrLDm", "/x030_152/fits/PrLDm/")
-plot_boltzmann_fits("01 aSf") %>% save_plot("10 PrLDm", "/x030_152/fits/PrLDm/")
-plot_boltzmann_fits("02 aSf") %>% save_plot("15 PrLDm", "/x030_152/fits/PrLDm/")
-plot_boltzmann_fits("04 aSf") %>% save_plot("20 PrLDm", "/x030_152/fits/PrLDm/")
+plot_boltzmann_fits("05 PrLDm") %>% save_plot("05 PrLDm", "/x030_152/fits/PrLDm/") # nolint
+plot_boltzmann_fits("10 PrLDm") %>% save_plot("10 PrLDm", "/x030_152/fits/PrLDm/") # nolint
+plot_boltzmann_fits("15 PrLDm") %>% save_plot("15 PrLDm", "/x030_152/fits/PrLDm/") # nolint
+plot_boltzmann_fits("20 PrLDm") %>% save_plot("20 PrLDm", "/x030_152/fits/PrLDm/") # nolint
